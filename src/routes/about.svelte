74dc7b7d1752903b100030e0426a1eaa5b7e56a2
fabezio <fabezio@outlook.fr>
2020-05-25 23:54:17 +0200

    ajout pages tarifs, portfolio / rédaction page a propos

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,5 +3,19 @@
 </svelte:head>
 
 <h1>A propos</h1>
+<h2>Qui nous sommes</h2>
 
-<p>Ce que vous avez besoin de savoir</p>
+<p>La création de sites et applications réactives n'est qu'une de nos prérogatives.
+Nous avons également à coeur de faire une présentation tant originale que présentable. En outre, un site web ne peut être pris au sérieux si l'assiduité et la lisibilité ne sont pas au rendez-vous, c'est pourquoi nous attachons beaucoup d'importance à la formulation.
+<br>Vous avez certainement pu constater le manque de rigueur apporté à certains sites, où l'aspect technique est très poussé, au détriment de l'orthographe. Or les deux ne doivent surtout pas être indissociables, bien qu'indépendantes. Le fond est aussi important que la forme, aussi nous proposons un service combinant les deux.
+<br>D'autre part, la technicité se veut des plus sobres afin de faciliter les tâches à accomplir, sans que les fonctionnalités en pâtissent. </p>
+<p>Dernier point, et non des moindres, l'avantage financier: en effet, la plupart des projets sont onéreux pour de mutlitiples raisons, dont l'ensemble des parties techniques à maitriser et le volume horaire qui en découle. La sobriété de nos applications vous exemptera de ces obligations. Il vous suffit de voir notre <a href="#">grille de tarifs</a> et le <a href="#">portfolio</a> pour vous en convaincre.
+<p>Vous aurez donc dans la concrétisation de votre projet les points suivants:</p>
+<ul>
+	<li>fonctionnalités optimales</li>
+	<li>sécurisation des données</li>
+	<li>sobriété de l'ensemble des interfaces</li>
+	<li>orthographe et grammaire irréprochables</li>
+	<li>réactivité</li>
+	<li>bon rapport qualité / prix</li>
+</ul>
---
+++ src/routes/skills.svelte
@@ -0,0 +1,7 @@
+<h1>Compétences</h1>
+<ul>
+  <li>Mise en place structure / contenu</li>
+  <li>Mise en forme</li>
+  <li>Création bases de données</li>
+  <li>Gestion requêtes serveur</li>
+</ul>
